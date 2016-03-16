b231fb4d93b17931fbfb69bbb9669778da795f40
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-03-16 15:04:58 -0400

    refactors and allows for listed feelings and times to eventually chart out

--- public/js/workouts/index.controller.js
+++ public/js/workouts/index.controller.js
@@ -1,20 +1,42 @@
 "use strict";
- console.log('controller');
+console.log('controller');
 
-  angular.module("workouts").controller("WorkoutIndexController", function (
-    $scope,
-    WorkoutFactory
-  ){
-    console.log('workout controller');
-    $scope.workouts = WorkoutFactory.query();
-    $scope.newWorkout = new WorkoutFactory();
-  });
+angular.module("workouts").controller("WorkoutIndexController", function (
+  $scope,
+  WorkoutFactory,
+  TagFactory
+){
 
-  angular.module("tags").controller("TagIndexController", function(
-    $scope,
-    TagFactory
-  ){
-    console.log('tags controller');
-    $scope.tags = TagFactory.query();
-    $scope.newTag = new TagFactory();
-  });
+  $scope.workoutArray = []
+  $scope.feelingsArray = []
+  $scope.timeArray = []
+
+
+  $scope.getResults = function(results){
+    for (var i = 0; i < results.length; i++){
+      var feelings = results[i].feelings;
+      var time = results[i].time;
+      var workout = results[i]
+      $scope.workoutArray.push(workout)
+      $scope.feelingsArray.push({'feelings':feelings})
+      $scope.timeArray.push({'time':time})
+    }
+    console.log($scope.workoutArray);
+  };
+
+  $scope.loadWorkout = function(workoutData){
+      console.log(workoutData);
+
+      $scope.workout = workoutData.data;
+      $scope.getResults($scope.workout);
+    };
+
+    WorkoutFactory.getWorkoutsData().then($scope.loadWorkout)
+
+  // $scope.workouts = WorkoutFactory.query();
+  // $scope.newWorkout = new WorkoutFactory();
+
+  $scope.tags = TagFactory.query();
+  $scope.newTag = new TagFactory();
+
+});
--- public/js/workouts/workouts.factory.js
+++ public/js/workouts/workouts.factory.js
@@ -1,17 +1,38 @@
-"use strict";
-console.log('factory');
-(function(){
-  angular
-  .module("workouts")
-  .factory("WorkoutFactory", [
-    "$resource",
-    WorkoutFactoryFunction
-  ]);
+// "use strict";
+// console.log('factory');
+// (function(){
+//   angular
+//   .module("workouts")
+//   .factory("WorkoutFactory", [
+//     "$resource",
+//     WorkoutFactoryFunction
+//   ]);
+//
+//   function WorkoutFactoryFunction($resource){
+//     console.log('workout factory');
+//     return $resource("/workouts/:id", {}, {
+//       update: {method: "PUT"}
+//     });
+//   }
+// }());
 
-  function WorkoutFactoryFunction($resource){
-    console.log('workout factory');
-    return $resource("/workouts/:id", {}, {
-      update: {method: "PUT"}
-    });
-  }
-}());
+"use strict";
+angular.module('workouts')
+  .factory('WorkoutFactory', function(
+    $http
+  ){
+
+  var service = {};
+
+  service.getWorkoutsData = function(){
+    console.log('getWorkoutData');
+    return $http.get('/workouts');
+  };
+
+  service.getOneWorkoutData = function(workout_id){
+    console.log('getWorkoutData');
+    return $http.get('/workouts/' + workout_id);
+  };
+
+  return service;
+});
